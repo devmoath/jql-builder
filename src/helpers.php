63ede81d6c50e5d94617c90cb287e1756721ee2a
Moath <moath.alhajrii@gmail.com>
2021-09-17 16:25:04 +0300

    add array_wrap helper function

--- src/helpers.php
+++ src/helpers.php
@@ -8,3 +8,14 @@ if (! function_exists('tap')) {
         return $value;
     }
 }
+
+if (! function_exists('array_wrap')) {
+    function array_wrap(mixed $value): array
+    {
+        if (is_null($value)) {
+            return [];
+        }
+
+        return is_array($value) ? $value : [$value];
+    }
+}
