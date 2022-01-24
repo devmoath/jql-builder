dafa3a6f080ea7be637718eb2564f13add604f86
Moath <moath.alhajrii@gmail.com>
2022-01-24 12:35:26 +0300

    create Keyword class

---
+++ src/Keyword.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DevMoath\JqlBuilder;
+
+final class Keyword
+{
+    /**
+     * @var string
+     * @see https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/#Advancedsearchingkeywordsreference-ANDAND
+     */
+    public const AND = 'and';
+
+    /**
+     * @var string
+     * @see https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/#Advancedsearchingkeywordsreference-OROR
+     */
+    public const OR = 'or';
+
+    /**
+     * @var string
+     * @see https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/#Advancedsearchingkeywordsreference-NOTNOT
+     */
+    public const NOT = 'not';
+
+    /**
+     * @var string
+     * @see https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/#Advancedsearchingkeywordsreference-EMPTYEMPTY
+     */
+    public const EMPTY = 'empty';
+
+    /**
+     * @var string
+     * @see https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/#Advancedsearchingkeywordsreference-NULLNULL
+     */
+    public const NULL = 'null';
+
+    /**
+     * @var string
+     * @see https://support.atlassian.com/jira-software-cloud/docs/advanced-search-reference-jql-keywords/#Advancedsearchingkeywordsreference-ORDER
+     */
+    public const ORDER_BY = 'order by';
+}
